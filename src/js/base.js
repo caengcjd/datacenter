5022cdab85c760493c9569b2de7d6d6d66abc94d
rainie <463103063@qq.com>
2016-02-21 11:06:43 +0800

    update

--- src/js/base.js
+++ src/js/base.js
@@ -1,4 +1,3 @@
-
 /////////////
 // base.js //
 /////////////
@@ -19,44 +18,57 @@ require('autotrack');
 /* ------------------------------------------------------------
  * google analytics
  * ------------------------------------------------------------ */
- $(function () {
-     window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-     ga('create', 'UA-74094643-1', 'auto');
-     ga('require', 'autotrack');
-     ga('send', 'pageview');
- })
+$(function() {
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] ||
+            function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            },
+            i[r].l = 1 * new Date();
+        a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'http//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-74094643-1', 'auto');
+    ga('require', 'autotrack');
+    ga('send', 'pageview');
+})
 
 
 /* ------------------------------------------------------------
-*	加载footer / 初始化footer
-* ------------------------------------------------------------ */
+ *	加载footer / 初始化footer
+ * ------------------------------------------------------------ */
 var footer = require('../tpl/footer.html');
-$(function () {
+$(function() {
     $('#footer').html(footer);
 })
 
 /* ------------------------------------------------------------
-*	加载navbar
-* ------------------------------------------------------------ */
+ *	加载navbar
+ * ------------------------------------------------------------ */
 
- var navbar = require('../tpl/navbar.html');
-$(function () {
+var navbar = require('../tpl/navbar.html');
+$(function() {
     $('#navbar').html(navbar);
 })
 
 
 /* ------------------------------------------------------------
-*  导航 下拉触发
-* ------------------------------------------------------------ */
+ *  导航 下拉触发
+ * ------------------------------------------------------------ */
 
- $(function () {
-     $(document).on('mouseenter', '.navbar .dropdown', function () {
-         $(this).find('.dropdown-toggle').dropdown('toggle');
-     })
-     $(document).on('mouseleave', '.navbar .dropdown', function () {
-         $(this).find('.dropdown-toggle').dropdown('toggle');
-     })
- })
+$(function() {
+    $(document).on('mouseenter', '.navbar .dropdown', function() {
+        $(this).find('.dropdown-toggle').dropdown('toggle');
+    })
+    $(document).on('mouseleave', '.navbar .dropdown', function() {
+        $(this).find('.dropdown-toggle').dropdown('toggle');
+    })
+})
 
 /* ------------------------------------------------------------
  * 返回顶部
@@ -67,6 +79,8 @@ function smoothScroll(btn, target) {
     var animationTime = 500;
     $(document).on('click', btn, function() {
         var position = $(target).offset().top;
-        $('html, body').animate({scrollTop: position}, animationTime);
+        $('html, body').animate({
+            scrollTop: position
+        }, animationTime);
     })
 }
